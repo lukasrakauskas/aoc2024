2d6cd6d5b1fa9d8cea23cea022f6297face27989
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2024-12-04 21:20:00 +0200

    Solve day 03 part 2

--- days/03.ts
+++ days/03.ts
@@ -1,42 +1,54 @@
-import { readInputLines } from "../utils.ts";
-
-// not 10598741
-// not 48810620
-// not 5309014
-
-// That's not the right answer; your answer is too low
-
 export async function solve() {
-  let sum = 0;
-  let sumWithDos = 0;
+  const data = await Deno.readTextFile("inputs/03.txt");
 
-  const from = `don't()`;
-  const to = `do()`;
-
-  const data = await Deno.readTextFile("inputs/03-test.txt");
-
-  const muls = [...data.matchAll(/mul\([0-9]+\,[0-9]+\)/gs)].map((it) =>
-    it[0].replace("mul(", "").replace(")", "").split(",").map(Number)
-  ).map((it) => it[0] * it[1]);
-
-  sum += muls.reduce((a, b) => a + b, 0);
-
-  const middleRemoved = data.replaceAll(/don\'t\(\).+do\(\)/gs, "");
-
-  // const endRemoved = middleRemoved.slice(middleRemoved.indexOf(from));
-
-  const muls2 = [
-    ...middleRemoved.matchAll(
-      /mul\([0-9]+\,[0-9]+\)/g,
+  const instructions = [
+    ...data.matchAll(/(mul\([0-9]+\,[0-9]+\))/g).map(
+      (it) => it[0],
     ),
-  ].map((it) =>
-    it[0].replace("mul(", "").replace(")", "").split(",").map((it) => {
-      console.log(it);
-      return parseInt(it);
-    })
-  ).map((it) => it[0] * it[1]);
+  ];
 
-  sumWithDos += muls2.reduce((a, b) => a + b, 0);
+  const instructionsWithToggle = [
+    ...data.matchAll(/(mul\([0-9]+\,[0-9]+\))|(don\'t\(\))|(do\(\))/g).map(
+      (it) => it[0],
+    ),
+  ];
 
-  return [sum, sumWithDos];
+  return [
+    processInstructions(instructions),
+    processInstructionsWithToggle(instructionsWithToggle),
+  ];
+}
+function processInstructions(instructions: string[]): number {
+  let sum = 0;
+
+  for (const instruction of instructions) {
+    const [x, y] = instruction.match(/\d+/g)?.map((it) => parseInt(it, 10)) ??
+      [0, 0];
+
+    sum += x * y;
+  }
+
+  return sum;
+}
+
+function processInstructionsWithToggle(instructions: string[]): number {
+  let sum = 0;
+  let enabled = true;
+
+  for (const instruction of instructions) {
+    if (instruction.startsWith("mul")) {
+      if (enabled) {
+        const [x, y] = instruction.match(/\d+/g)?.map((it) =>
+          parseInt(it, 10)
+        ) ?? [0, 0];
+        sum += x * y;
+      }
+    } else if (instruction === `don't()`) {
+      enabled = false;
+    } else if (instruction === "do()") {
+      enabled = true;
+    }
+  }
+
+  return sum;
 }
